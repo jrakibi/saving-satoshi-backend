80ad9fc36be1ce1ea41c692bd5166c980ade30b3
jrakibi <j.errakibi@gmail.com>
2026-01-05 17:08:59 +0700

    fix: update terraform version

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
   }
 
-  required_version = ">= 1.14"
+  required_version = ">= 1.5"
 }
 
 provider "aws" {
